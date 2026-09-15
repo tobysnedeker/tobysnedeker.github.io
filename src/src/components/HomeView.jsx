1faab19c8f8c963523fe48397e99bc31512cee0f
Toby Snedeker <tesnedek@ncsu.edu>
2026-09-15 18:27:37 -0400

    updated home message

--- src/src/components/HomeView.jsx
+++ src/src/components/HomeView.jsx
@@ -14,7 +14,7 @@ const HomeView = () => {
           fontSize: '18px',
           fontWeight: '400'
         }}>
-          Nice to meet you! I'm Toby Snedeker, a student at NC State University with a major in Computer Science and minor in Brewing Science. I'm passionate about many more things,
+          Hello! I'm Toby Snedeker, a student at NC State University with a major in Computer Science and minor in Brewing Science. I'm passionate about many more things,
           including rock climbing, cars, movies, and (especially) photography. I made this space to show off some of my favorite shots while exercising my web skills. To top it off, I 
           picked up some new skills along the way, like using GitHub Actions to rebuild this page whenever I update the list of photos I want to display!
         </p>
